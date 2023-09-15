c58a56891e6e78c9a13cd0135d76634f47b3d66d
ycbencheng <ycbencheng@gmail.com>
2023-09-15 10:58:50 -0600

    Create a scraper

--- scraper.rb
+++ scraper.rb
@@ -6,10 +6,10 @@ require "byebug"
 class Scraper
   def initialize(csv)
     @csv = csv
-    @company = Struct.new(:name, :email, :site, :facebook, :phone)
+    @company = Struct.new(:name, :email, :site, :facebook, :phone, :error)
     @companies = []
     @semaphore = Mutex.new
-    @csv_headers = %w(name, email, site, facebook, phone)
+    @csv_headers = %w(name, email, site, facebook, phone, error)
     @body = nil
   end
 
@@ -29,18 +29,20 @@ class Scraper
         if html.code < 400
           @body = Nokogiri::HTML(html.body)
 
-          # n = parse_name
+          name = parse_name
           email = parse_email
           facebook = parse_fb
           phone = parse_phone
         else
-          puts "Warning error code - #{html.code}"
+          error = "Error Code - #{html.code}"
         end
       rescue StandardError => e
-        puts "Warning #{site} is not available! Error - #{e.message}"
+        error = "Connection Error - #{e.message}"
       end
 
-      company_struct = @company.new('', email, site, facebook, phone)
+      puts error ? error : "Got some info on the company"
+
+      company_struct = @company.new(name, email, site, facebook, phone, error)
 
       @semaphore.synchronize {
         @companies.push(company_struct)
@@ -48,6 +50,10 @@ class Scraper
     end
 
     puts "Total of #{@companies.count}"
+
+    # dedup the site
+    # skip if the site is mountainstar, ihc, stmarks, intermountainhealthcare, .gov, .edu, reverehealth, lenscrafter, target, walmart
+    # check if the phone number, website is the same
 
     create_csv
   end
@@ -59,7 +65,6 @@ class Scraper
 
     CSV.open("#{@csv}_info.csv", "wb", write_headers: true, headers: @csv_headers) do |csv|
       @companies.each do |company|
-        puts "Writing #{company.site} & #{company.email}"
         csv << company
       end
     end
@@ -76,36 +81,51 @@ class Scraper
   end
 
   def parse_name
-    # parse for dr name or manager, or front desk
-    data = extractor(selector("//a[starts-with(@href, \"#{label}\")]/@href"))
-    cleaned_data = names
+    data = extractor("//*[contains(*, \"dr\")]")
+
+    _, raw_name = data.text.downcase.split('dr.')
+
+    cleaned_data = if raw_name.nil?
+                     nil
+                   elsif raw_name.split().first == '.'
+                     raw_name.split().first(2).unshift('Dr').insert(2, " ").map{|rn| rn.capitalize}.join()
+                   else
+                    raw_name.split().first(1).unshift('Dr').insert(1, " ").map{|rn| rn.capitalize}.join()
+                   end
+
     logging(__method__, cleaned_data)
+
+    cleaned_data
   end
 
   def parse_email
     data = extractor(href_selector("mailto:"))
+    #look for span, div, p
     cleaned_data = if data.any?
-             data.collect {|n| n.value[7..-1]}.uniq.first
-           else
-             selector = "//*[contains(*, '.com')]"
-             data = extractor(selector)
-             emails = data.text.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[com]{2,4}\b/i)
-             emails.uniq.first
-           end
+                     data.collect {|n| n.value[7..-1]}.uniq.first
+                   else
+                     data = extractor("//*[contains(*, '.com')]")
+                     emails = data.text.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[com]{2,4}\b/i)
+                     emails.uniq.first
+                   end
 
     logging(__method__, cleaned_data)
+
     cleaned_data
   end
 
   def parse_fb
     data = extractor(href_selector("https://www.facebook.com"))
     cleaned_data = data.map(&:to_s).uniq.first if data.any?
+
     logging(__method__, cleaned_data)
+
     cleaned_data
   end
 
   def parse_phone
     data = extractor(href_selector("tel:"))
+    # clean phone
     cleaned_data = data.collect {|n| n.value[4..-1]}.uniq.first if data.any?
     logging(__method__, cleaned_data)
     cleaned_data
